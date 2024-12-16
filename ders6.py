1991592ad6147b2382453cec49a0a90c51b1edcb
Furkan "Nafair <115067547+furkandlkdr@users.noreply.github.com>
2024-12-16 23:09:39 +0300

    Create ders6.py

---
+++ ders6.py
@@ -0,0 +1,23 @@
+import math
+a=math.fabs(-5)
+b=abs(-9)
+print(a)
+print(b)
+
+b=2.51
+print(math.ceil(b))
+print(math.floor(b))
+print(round(b))
+
+c=math.pow(3,4)
+print(c)
+
+d=math.sqrt(25)
+print(d)
+
+g=math.log(10,2)
+print(g)
+liste=[3,6,8,4,2,9]
+print(min(liste))
+print(max(liste))
+print(sum(liste))
